b59e3bb1b7557cd3694988b9e444c68bcc7b6fcf
Center <>
2022-07-16 03:20:16 +0700

    fix

--- index.ts
+++ index.ts
@@ -1,20 +1,42 @@
 // common interface
-export * as IAds from "./interfaces/commons/ads";
-export * as IBackground from "./interfaces/commons/background";
-export * as IContact from "./interfaces/commons/contact";
-export * as IFont from "./interfaces/commons/font";
-export * as INavigateLink from "./interfaces/commons/navigate_link";
-export * as IPointType from "./interfaces/commons/point_type";
-export * as ISeo from "./interfaces/commons/seo";
-export * as IUnit from "./interfaces/commons/unit";
+import * as IAds from "./interfaces/commons/ads";
+import * as IBackground from "./interfaces/commons/background";
+import * as IContact from "./interfaces/commons/contact";
+import * as IFont from "./interfaces/commons/font";
+import * as INavigateLink from "./interfaces/commons/navigate_link";
+import * as IPointType from "./interfaces/commons/point_type";
+import * as ISeo from "./interfaces/commons/seo";
+import * as IUnit from "./interfaces/commons/unit";
 
 // big interface
-export * as ICatalogue from "./interfaces/commons/catalogue";
-export * as IChildCatalogue from "./interfaces/commons/catalogue/child_catalogue";
-export * as IDetailCatalogue from "./interfaces/commons/catalogue/detail_catalogue";
+import * as ICatalogue from "./interfaces/commons/catalogue";
+import * as IChildCatalogue from "./interfaces/commons/catalogue/child_catalogue";
+import * as IDetailCatalogue from "./interfaces/commons/catalogue/detail_catalogue";
 
-export * as ICategory from "./interfaces/commons/category";
-export * as IUser from "./interfaces/commons/user";
+import * as ICategory from "./interfaces/commons/category";
+import * as IUser from "./interfaces/commons/user";
 
 // page interface
-export * as ICourse from "./interfaces/course";
+import * as ICourse from "./interfaces/course";
+
+export default {
+  // common interface
+  IAds,
+  IBackground,
+  IContact,
+  IFont,
+  INavigateLink,
+  IPointType,
+  ISeo,
+  IUnit,
+
+  // big interface
+  ICatalogue,
+  IChildCatalogue,
+  IDetailCatalogue,
+  ICategory,
+
+  IUser,
+
+  ICourse
+}
